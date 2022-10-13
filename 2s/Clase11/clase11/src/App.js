371c64ae58174c2a7da7265b9354a38dda2835c8
Angcelo <anmaramaga@gmail.com>
2022-10-13 10:30:41 -0600

    Clase 11

---
+++ 2s/Clase11/clase11/src/App.js
@@ -0,0 +1,28 @@
+//import logo from './logo.svg';
+import './App.css';
+import { useRoutes } from 'react-router-dom';
+import Home from './Vistas/Home/Home';
+import About from './Vistas/About/About';
+
+function Routes() {
+  return useRoutes([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: 'about',
+      element: <About />
+    }
+  ])
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2s/Clase11/clase11/src/Vistas/Home/Home.js
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Button variant="secondary">Primary</Button>
+        </div>
+    )
+}
+
+export default Home;
